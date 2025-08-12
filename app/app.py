58c3d518842ab02b4428a02f2abb6a4e0c5fe348
Marc Mendoza <777marc@gmail.com>
2025-08-12 11:38:58 -0400

    add users, env

--- app/app.py
+++ app/app.py
@@ -1,13 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
 
 db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./site.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///./site.db')
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev-key-please-change')
     
     db.init_app(app)
 
--- app/models.py
+++ app/models.py
@@ -11,3 +11,19 @@ class Person(db.Model):
 
     def __repr__(self):
         return f'Person Name: {self.name} age: {self.age} job: {self.job}'
+    
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    uid = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.Text, nullable=False)
+    password = db.Column(db.Text, nullable=False)
+    role = db.Column(db.Text)
+    description = db.Column(db.Text)
+
+    def __repr__(self):
+        return f'User Name: {self.username} role: {self.role}'
+
+    def get_id(self):
+        return self.uid
